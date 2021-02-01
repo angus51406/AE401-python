86987f9d8509a6dbd1625c85e1841a3533fd5bd1
angus51406 <68845868+angus51406@users.noreply.github.com>
2021-02-01 16:51:45 +0800

    Add files via upload

---
+++ 圓周率.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 14:07:34 2021
+
+@author: user
+"""
+
+r = float(input('請輸入圓的半徑'))
+x=float(3.1415926)
+print('圓周率為',x)
+print('圓周長為',r*2*x)
+print('圓面積為',r*r*x)
