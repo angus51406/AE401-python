cf8c571b11e6417904574a9b6e47356499a8926e
angus51406 <68845868+angus51406@users.noreply.github.com>
2021-02-03 16:54:57 +0800

    Add files via upload

---
+++ list2.py
@@ -0,0 +1,25 @@
+scores=[]
+n=int(input('請輸入班上有幾人'))
+max=0
+min=101
+e=0
+for i in range(n):
+    
+    s=str(input('請輸入姓名'))
+    v=int(input('請輸入分數'))
+    a=int(input('請輸入年齡'))
+    if v>max:
+       max=v
+       maxnum=i
+    if v<min:
+       min=v   
+       minnum=i
+    e=e+v   
+    oneperson=list()   
+    oneperson.append(s)
+    oneperson.append(v)
+    oneperson.append(a)
+    scores.append(oneperson)
+print('最高分:',max,'最低分:',min)
+print('最高分的人:',scores[maxnum][0],scores[maxnum][2],'歲''最低分的人:',scores[minnum][0],scores[minnum][2],'歲')
+print('平均分數是',e/n)
